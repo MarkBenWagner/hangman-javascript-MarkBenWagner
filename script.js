e84a028ce518eb6064188be7edeecd6a1e78971a
Mark B. Wagner <wagnermarkbenjamin@gmail.com>
2023-01-21 17:33:42 +0100

    user input is working fine

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const prompt = require('prompt-sync')();
 let wordslist = randomWords(100);
 let myWord = wordslist[Math.floor(Math.random() * 101)];
 console.log(myWord);
-console.log(myWord.length);
+// console.log(myWord.length);
 let line = "";
 for (let i = 0; i < myWord.length; i++){
     line += "_ ";
 }
 console.log(line);
+let lineArr = Array.from(line);
+console.log(lineArr);
+lineArr = lineArr.replace(' ', '');
+
+
 let userInput = prompt("Choose a letter: ");
-console.log(userInput);
+// console.log(userInput);
+let arr = Array.from(myWord);
+// console.log(arr);
+while (userInput.length > 1){
+    console.log("You should write only one letter! \nTry again! \n");
+    userInput = prompt("Choose a letter: ");
+}
+
+// else..
+
+for (let j = 0; j < arr.length; j++){
+    if (arr[j].includes(userInput)){
+        console.log(userInput);
+    }
+}
+console.log(typeof lineArr);
+console.log(lineArr[3]);
